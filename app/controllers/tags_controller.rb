90c104afa999dd6856421a152531d3ee558982c4
Austin <austinfpdx@gmail.com>
2018-04-21 13:03:54 -0700

    Tagging feature complete

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,16 @@
+class TagsController < ApplicationController
+	def show
+		@tag = Tag.find( params[:id] )
+	end 
+	def index         #exp 
+		@tags = Tag.all 
+	end 
+	def destroy 
+		@tag=Tag.find( params[:id] )
+		@tag.destroy 
+
+		redirect_back(fallback_location: root_path)
+
+
+	end 
+end
